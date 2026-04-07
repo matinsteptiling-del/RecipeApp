4eec15d0ab244e780923407ec2ac15ff0c3cea77
md <md@outlook.com>
2026-04-07 10:49:23 +1000

    Updated os.getenv default value & updated app.py

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,10 @@
-from flask import Flask 
+import os
+from flask import Flask
+from dotenv import load_dotenv 
+
+# Load variables from the .env file
+load_dotenv()
+
 # Initialize the Flask application
 app = Flask(__name__)
 
@@ -9,4 +15,7 @@ def hello_world():
 
 # Run the app if this script is executed directly
 if __name__ == '__main__':
-    app.run(debug=True)
+
+# Returns '5000' if 'PORT' is not set
+    port = os.getenv('PORT', '5000')
+    app.run (port=port, debug=True)
